358e18b3ef06de55da5621ba9abddb3e5a796017
chick-p <chick.p.1120@gmail.com>
2019-08-27 11:58:27 +0900

    Changed: allow POST method

--- src/algolia.js
+++ src/algolia.js
@@ -8,59 +8,42 @@ const API_KEY = process.env.ADMIN_API_KEY;
 
 const algoliasearch = require('algoliasearch');
 
-exports.handler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    return {
-      statusCode: 503,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({error: 'Method Not Allowed'})
-    }
+const createResponse = (status, body) => {
+  return {
+    headers: {
+      'content-type': 'application/json; charset=utf-8'
+    },
+    statusCode: status,
+    body: JSON.stringify(body)
+  };
+}
+
+exports.handler = async (event, context, callback) => {
+  if (!event.httpMethod === ('GET' || 'POST')) {
+    callback(null, createResponse(503, {
+      error: 'Method not allowed'
+    }));
   }
+
   const indexName = event.queryStringParameters.name;
   const indexPath = event.queryStringParameters.path;
-  console.log(indexPath);
-
   if (!indexName || !indexPath ) {
-    return {
-      statusCode: 503,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({error: 'Require \'index\'(index name) and \'path\'(index json path)'})
-    }
+    callback(null, createResponse(503, {
+      error: 'Require index and path'
+    }));
   }
 
   try {
     const client = algoliasearch(APP_ID, API_KEY);
     const index = client.initIndex(indexName);
-
     const content = await rp(indexPath);
     const resp = await index.addObjects(JSON.parse(content));
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({message: resp})
-    }
+    callback(null, createResponse(200, {
+      message: resp
+    }));
   } catch (e) {
-    console.log(e);
-    return {
-      statusCode: 503,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'content-type': 'application/json; charset=utf-8',
-      },
-      body: JSON.stringify({error: e})
-    }
+    callback(null, createResponse(503, {
+      error: e
+    }));
   }
 };
